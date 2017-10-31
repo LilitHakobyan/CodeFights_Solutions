ecba687bd0c85678ae969d05497985fa3bb7d6b7
lilithakobyan <lilithakobyan1994@mail.ru>
2017-10-31 23:23:24 +0400

    (Y)

---
+++ Intro/AdjacentElementsProduct/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdjacentElementsProduct
+{
+    class Program
+    {
+        static  int AdjacentElementsProduct(int[] inputArray)
+        {
+            int largestProduct = 0;
+            int thisProduct = 0;
+            for (int i = 0; i < inputArray.Length-1; i++)
+            {
+                thisProduct = inputArray[i] * inputArray[i + 1];
+                if (thisProduct>largestProduct)
+                {
+                    largestProduct = thisProduct;
+                }
+            }
+            return largestProduct;
+
+        }
+        static void Main(string[] args)
+        {
+            int [] inputArray =  { 3, 6, -2, -5, 7, 3};
+            Console.WriteLine(AdjacentElementsProduct(inputArray));
+            Console.ReadKey();
+        }
+    }
+}
