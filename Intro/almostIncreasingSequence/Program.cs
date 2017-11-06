f62f586d55636a6b4a5bae35fc3c7b791e8dc382
lilithakobyan <lilithakobyan1994@mail.ru>
2017-11-06 23:03:41 +0400

    ujs

---
+++ Intro/almostIncreasingSequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace almostIncreasingSequence
+{
+    class Program
+    {
+        public static bool almostIncreasingSequence(int[] sequence)
+        {
+            int count = 0;
+            for (int i = 0, j = 1; j < sequence.Length; j++)
+            {
+                if (sequence[i] >= sequence[j])
+                {
+                    count++;
+                    if (j > 1)
+                    {
+                        if (sequence[i - 1] < sequence[j])
+                        {
+                            i = j;
+                            continue;
+                        }
+                        continue;
+                    }
+                }
+                i = j;
+            }
+
+            return count <= 1;
+        }
+         static void Main(string[] args)
+        {
+            int[] seq = { 1, 2, 5, 3, 5};
+            Console.WriteLine(almostIncreasingSequence(seq));
+        }
+    }
+}
---
+++ Intro/makeArrayConsecutive2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeArrayConsecutive2
+{
+    class Program
+    {
+        public static int makeArrayConsecutive2(int[] statues)
+        {
+           int min = statues.Min();
+           int max = statues.Max();
+            int count = 0;
+            while (min!=max)
+            {
+                if (!statues.Contains(min+1))
+                {
+                   count++;
+                }
+                min = min + 1;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int[] statues = {6, 2, 3, 8};
+            Console.WriteLine(makeArrayConsecutive2(statues));
+            
+        }
+    }
+}
