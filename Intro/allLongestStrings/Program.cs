8f630f01f5530c20fc8b16eafa70c23d5440f29c
lilithakobyan <lilithakobyan1994@mail.ru>
2017-11-07 00:15:47 +0400

    yuhu

---
+++ Intro/allLongestStrings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allLongestStrings
+{
+    class Program
+    {
+        public static  string[] allLongestStrings(string[] inputArray)
+        {
+            List<string> newArrayList=new List<string>();
+            int maxSize =inputArray[0].Length;
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length>maxSize)
+                {
+                    maxSize = inputArray[i].Length;
+                }
+            }
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length==maxSize)
+                {
+                    newArrayList.Add(inputArray[i]);
+                }
+            }
+            string[] newArray = newArrayList.ToArray();
+            return newArray;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string[] arrayStrings = {"aba", "aa", "ad", "vcd", "aba"};
+            string[] newStringsarray = allLongestStrings(arrayStrings);
+            for (int i = 0; i < newStringsarray.Length; i++)
+            {
+                Console.Write(newStringsarray[i]+" , ");
+            }
+        }
+    }
+}
