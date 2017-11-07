e59336c5b661d3c77bd6bee05d905b497ceb81f1
lilithakobyan <lilithakobyan1994@mail.ru>
2017-11-08 00:24:24 +0400

    2 commits

---
+++ Intro/commonCharacterCount/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commonCharacterCount
+{
+    class Program
+    {
+        public static int commonCharacterCount(string s1, string s2)
+        {
+            int count = 0;
+            List<int> s2Set=new List<int>();
+            for (int i = 0; i < s2.Length; i++)
+            {
+                s2Set.Add(s2[i]);
+            }
+            for (int i = 0; i < s1.Length; i++)
+            {
+
+                if (s2Set.Contains(s1[i]))
+                {
+                    s2Set.Remove(s1[i]);
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            string s1 = "aabcc";
+            string s2 = "adcaa";
+            Console.WriteLine(commonCharacterCount(s1, s2));
+        }
+    }
+}
---
+++ Intro/isLucky/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isLucky
+{
+    class Program
+    {
+        public  static bool isLucky(int n)
+        {
+           ArrayList listOfDigets=new ArrayList();
+            int sum1 = 0;
+            int sum2 = 0;
+            while (n!=0)
+            {
+                listOfDigets.Add(n % 10);
+                n = n / 10;
+                
+            }
+            for (int i = 0; i < listOfDigets.Count/2; i++)
+            {
+                sum1 +=(int)listOfDigets[i];
+                sum2 += (int) listOfDigets[listOfDigets.Count - i-1];
+            }
+            return sum2==sum1;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 239017;
+            Console.WriteLine(isLucky(n));
+        }
+    }
+}
