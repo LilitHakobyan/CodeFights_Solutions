170255da3404cb64821af82e69f85e1f67f56eb3
lilithakobyan <lilithakobyan1994@mail.ru>
2018-01-05 18:44:11 +0400

    /

--- Intro/variableName/Program.cs
+++ Intro/variableName/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace variableName
@@ -9,10 +10,23 @@ namespace variableName
     class Program
     {
        public static  bool variableName(string name)
+       {
+           if (char.IsDigit(name, 0))
+               return false;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(name, i) )
+                { if(name[i] != '_')
+                    return false;
+                }
+            }
+           return true;
+       }
+        bool variableName1(string name)
         {
-
+            var regex = new Regex(@"^[a-zA-Z_][a-zA-Z_\d]*$");
+            return regex.IsMatch(name);
         }
-
         static void Main(string[] args)
         {
             string name = "var_1__Int";
