380fa5003c1c31b38112100e62a1736a06be05b2
lilithakobyan <lilithakobyan1994@mail.ru>
2018-02-11 13:51:47 +0400

    new level

---
+++ Intro/Land of Logic 52-60/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Land_of_Logic_52_60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
